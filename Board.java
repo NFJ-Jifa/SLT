b21d6152ae174775fc9a0984300eadbe1d8ddcc0
muesli01 <wi23b123@technikum-wien.at>
2024-06-01 17:24:30 +0200

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,56 @@
+package at.technikum;
+
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("▁▁▁▁▁▁▁");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] == ' ' ? " " : cells[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+        System.out.println("▔▔▔▔▔▔▔");
+    }
+
+    public char[][] getCells() {
+        return cells;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,13 @@
+package at.technikum;
+
+public class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
