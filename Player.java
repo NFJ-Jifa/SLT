63f64917941c0bd02a7098f7b09384adee0c6d91
muesli01 <wi23b123@technikum-wien.at>
2024-06-01 18:06:04 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,12 +2,12 @@ package at.technikum;
 
 public class Player {
     private char marker;
-
+   // Constructor to initialize the player with a given marker
     public Player(char marker) {
-        this.marker = marker;
+        this.marker = marker;// Assign the marker to the instance variable
     }
-
+    // Method to retrieve the marker of the player
     public char getMarker() {
-        return marker;
+        return marker;// Return the player's marker
     }
 }
